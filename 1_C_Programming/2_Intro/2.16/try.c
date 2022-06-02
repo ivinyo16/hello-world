10e23c4edb5b9bafe0922ef6f30e1944ec1f7211
Ivin Lim <ivin.lim16@gmail.com>
2022-06-02 12:21:53 -0700

    a demo to show to sabrina for learning

--- 1_C_Programming/2_Intro/2.16/try.c
+++ 1_C_Programming/2_Intro/2.16/try.c
@@ -5,11 +5,14 @@
 int main(int argc, char* argv[])
 {
 	int a, b;
+	printf("please enter value for a:");
 	scanf("%d", &a);
+	printf("please enter value for b:");
 	scanf("%d", &b);
 	printf("\nsum is %d\n", a + b);
 	printf("difference is %d\n", a - b);
 	printf("product is %d\n", a * b);
 	printf("quotient is %d\n", a / b);
 	printf("remainder is %d\n", a % b);
+	printf("the difference of b to a  is %d\n", b - a);
 }
