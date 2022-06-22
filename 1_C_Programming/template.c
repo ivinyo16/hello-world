4a4e04a374f0ab11ff67127c457c6932627fb19f
Ivin Lim <ivin.lim16@gmail.com>
2022-06-22 15:08:53 -0700

    update template

--- 1_C_Programming/template.c
+++ 1_C_Programming/template.c
@@ -1,3 +1,15 @@
+/**
+ * @file template.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+/* Standard libraries */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,7 +17,6 @@
 int main(int argc, char* argv[])
 {
 
-	
 
-	return 0;
+    return 0;
 }
