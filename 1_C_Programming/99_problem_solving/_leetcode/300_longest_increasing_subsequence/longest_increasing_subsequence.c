60b91020000f02f4924f26c80f32aa37ca8ac166
Ivin Lim <ivin.lim16@gmail.com>
2022-08-08 02:47:52 -0700

    longest increasing subsequence using various DP and binary search techniques
    Signed-off-by: Ivin Lim <ivin.lim16@gmail.com>

---
+++ 1_C_Programming/99_problem_solving/_leetcode/300_longest_increasing_subsequence/longest_increasing_subsequence.c
@@ -0,0 +1,94 @@
+/**
+ * @file longest_increasing_subsequence.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * 
+ * Given an integer array nums, return the length of the longest strictly increasing subsequence.
+ * 
+ * A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the 
+ * remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+ * 
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+ * Example 2:
+ * 
+ * Input: nums = [0,1,0,3,2,3]
+ * Output: 4
+ * Example 3:
+ * 
+ * Input: nums = [7,7,7,7,7,7,7]
+ * Output: 1
+ *  
+ * 
+ * Constraints:
+ * 
+ * 1 <= nums.length <= 2500
+ * -104 <= nums[i] <= 104
+ * 
+ * @version 0.1
+ * @date 2022-08-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+/* Standard libraries */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+
+#define DEBUG
+#ifdef DEBUG
+#define DEBUG_PRINT(...) do{ fprintf( stderr, __VA_ARGS__ ); } while( false )
+#else
+#define DEBUG_PRINT(...) do{ } while ( false )
+#endif
+
+
+int lengthOfLISAux(int *nums, int numsSize,  int index, int prev)
+{
+    int ans = 0;
+    int dont_take = 0;
+    int take = 0;
+    if(index >= numsSize)
+    {
+        return 0;
+    }
+    dont_take = lengthOfLISAux(nums, numsSize, index + 1, prev);
+    if(nums[index] > prev)
+    {
+        take = 1 + lengthOfLISAux(nums, numsSize, index + 1, nums[index]);
+    }
+    ans = take >= dont_take ? take : dont_take;
+
+    return ans;
+
+
+}
+
+int lengthOfLIS(int* nums, int numsSize)
+{
+    return lengthOfLISAux(nums, numsSize, 0, INT_MIN);
+}
+
+int main(int argc, char* argv[])
+{
+    // int nums[] = {10 , 9 , 2 , 5 , 3, 7 , 101 , 18};
+    // int nums[] = {10 , 9 , 4 , 2 , 3, 1 , 7 , 11, 18};
+    int nums[] =  {4,10,4,3,8,9};
+    int size = sizeof(nums)/sizeof(nums[0]);
+    int answer = lengthOfLIS(nums, size);
+
+    printf("longest subsequence: %d\n", answer);
+
+
+
+    return 0;
+}
---
+++ 1_C_Programming/99_problem_solving/_leetcode/300_longest_increasing_subsequence/top_down_longest_increasing_subsequence.c
@@ -0,0 +1,135 @@
+/**
+ * @file longest_increasing_subsequence.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * 
+ * Given an integer array nums, return the length of the longest strictly increasing subsequence.
+ * 
+ * A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the 
+ * remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+ * 
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+ * Example 2:
+ * 
+ * Input: nums = [0,1,0,3,2,3]
+ * Output: 4
+ * Example 3:
+ * 
+ * Input: nums = [7,7,7,7,7,7,7]
+ * Output: 1
+ *  
+ * 
+ * Constraints:
+ * 
+ * 1 <= nums.length <= 2500
+ * -104 <= nums[i] <= 104
+ * 
+ * @version 0.1
+ * @date 2022-08-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+/* Standard libraries */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+
+#define DEBUG
+#ifdef DEBUG
+#define DEBUG_PRINT(...) do{ fprintf( stderr, __VA_ARGS__ ); } while( false )
+#else
+#define DEBUG_PRINT(...) do{ } while ( false )
+#endif
+
+
+void printMatrix(int m, int n, int matrix[][n])
+{
+    int i,j;
+    for( i = 0 ; i < m ; i++ )
+    {
+        for( j = 0 ; j < n ; j++ )
+        {
+            printf("%d, ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/**
+ * @brief 
+ * 
+ * @param nums array in question
+ * @param numsSize size of array
+ * @param index current starting index
+ * @param prev_i 
+ * @param dp 
+ * @return int 
+ */
+int lengthOfLISAux(int *nums, int numsSize,  int index, int prev_i, int dp[][numsSize+1])
+{
+    int ans = 0;
+    int dont_take = 0;
+    int take = 0;
+
+    if(index >= numsSize)
+    {
+        return 0;
+    }
+    if(dp[index][prev_i+1] != -1)
+    {
+        return dp[index][prev_i+1];
+    }
+    dont_take = lengthOfLISAux(nums, numsSize, index + 1, prev_i, dp);
+    if(prev_i == -1 || nums[index] > nums[prev_i])
+    {
+        take = 1 + lengthOfLISAux(nums, numsSize, index + 1, index, dp);
+    }
+    dp[index][prev_i+1] = (take >= dont_take) ? take : dont_take;
+    DEBUG_PRINT("%d %d\n", index, prev_i+1);
+    return dp[index][prev_i+1];
+
+
+}
+
+int lengthOfLIS(int* nums, int numsSize)
+{
+    int dp[numsSize][numsSize+1];
+    for(int i = 0; i < numsSize ; i++)
+    {
+        for(int j = 0; j < numsSize+1 ; j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
+
+    // return lengthOfLISAux(nums, numsSize, 0, -1, dp);
+    int answer =  lengthOfLISAux(nums, numsSize, 0, -1, dp);
+    printMatrix(numsSize, numsSize+1, dp);
+    return answer;
+}
+
+int main(int argc, char* argv[])
+{
+    // int nums[] = {10 , 9 , 2 , 5 , 3, 7 , 101 , 18};
+    // int nums[] = {10 , 9 , 4 , 2 , 3, 1 , 7 , 11, 18};
+    // int nums[] =  {4,10,4,3,8,9};
+    int nums[] = {1,2,4};
+    int size = sizeof(nums)/sizeof(nums[0]);
+    int answer = lengthOfLIS(nums, size);
+
+    printf("longest subsequence: %d\n", answer);
+
+
+
+    return 0;
+}
